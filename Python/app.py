29107e83c63552e477f78506935b0e88b039a167
tanushreenerella <tanushreenerella697@gmail.com>
2026-04-02 04:12:27 +0530

    fixes

--- Python/app.py
+++ Python/app.py
@@ -112,6 +112,5 @@ def predict():
     return render_template("form.html", prediction=prediction, graph=graph_generated)
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
