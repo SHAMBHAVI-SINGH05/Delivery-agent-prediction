07af0e5fa14b8c151ee60430d683ea72a2d519e5
tanushreenerella <tanushreenerella697@gmail.com>
2026-04-02 05:00:13 +0530

    path fixed

--- Python/app.py
+++ Python/app.py
@@ -7,7 +7,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 FEATURE_COLUMNS = [
     "Date",
